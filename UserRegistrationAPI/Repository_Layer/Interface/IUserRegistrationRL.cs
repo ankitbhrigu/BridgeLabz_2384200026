4b08d9edcf231421b812adecf29fb54216c3bce7
Ankit singh <ankitbhrigu22@gmail.com>
2025-03-03 21:14:53 +0530

    API Swagger and Logger Implementation Assignment uploaded

---
+++ UserRegistrationAPI/Repository_Layer/Interface/IUserRegistrationRL.cs
@@ -0,0 +1,14 @@
+﻿using Repository_Layer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository_Layer.Interface
+{
+    public interface IUserRegistrationRL
+    {
+        void RegisterUser(UserEntity user);
+    }
+}
---
+++ UserRegistrationAPI/Repository_Layer/Service/UserRegistrationRL.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using Repository_Layer.Context;
+using Repository_Layer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using Repository_Layer.Interface;
+
+namespace Repository_Layer.Service
+{
+    public class UserRegistrationRL : IUserRegistrationRL
+    {
+        private readonly UserContext _dbContext;
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public UserRegistrationRL(UserContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void RegisterUser(UserEntity user)
+        {
+            try
+            {
+                _dbContext.Users.Add(user);
+                _dbContext.SaveChanges();
+                logger.Info("User registered successfully: " + user.Email);
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error in RegisterUser: " + ex.Message);
+                throw;
+            }
+        }
+    }
+}
