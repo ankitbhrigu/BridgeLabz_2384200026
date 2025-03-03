1537c3f2592ba5c025a29fb64c96b7d6cf4a99d3
Ankit singh <ankitbhrigu22@gmail.com>
2025-03-03 12:05:00 +0530

    API assignment uploaded

---
+++ UserRegistrationAPI/BusinessLayer/Service/UserService.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Interface;
+using Repository_Layer.Entity;
+using Repository_Layer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using Microsoft.Extensions.Logging;
+using Repository_Layer.Service;
+
+
+namespace BusinessLayer.Service
+{
+    public class UserRegistrationBL : IUserRegistrationBL
+    {
+        private readonly IUserRegistrationRL _userRl;
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public UserRegistrationBL(IUserRegistrationRL _userRl)
+        {
+            this._userRl = _userRl;
+        }
+
+        public void Register(UserEntity user)
+        {
+            try
+            {
+                _userRl.RegisterUser(user);
+                logger.Info("User registered in Business Layer: " + user.Email);
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Error in Register: " + ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ UserRegistrationAPI/Repository_Layer/Context/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository_Layer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository_Layer.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+        public DbSet<UserEntity> Users { get; set; }
+
+    }
+}
